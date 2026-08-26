92c7c2582345e14e6b513526ae6fbd7f5afce0ba
ReeceSolana <110007863+reeceba@users.noreply.github.com>
2026-08-26 21:31:28 +1000

    Bust cache for cart control fix

--- countries.js
+++ countries.js
@@ -1,2 +1,2 @@
 // MINTFORGE worldwide country/listing/variant upgrade loader
-['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js','buyer-experience.js','cart-controls-fix.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-6';s.defer=false;document.head.appendChild(s)});
+['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js','buyer-experience.js','cart-controls-fix.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-7';s.defer=false;document.head.appendChild(s)});
