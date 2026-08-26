cfffcdb7d82284b365082c2ca86c53ed8d5375fd
ReeceSolana <110007863+reeceba@users.noreply.github.com>
2026-08-26 21:38:54 +1000

    Replace fragile cart controls with cart experience v2

--- countries.js
+++ countries.js
@@ -1,2 +1,2 @@
-// MINTFORGE worldwide country/listing/variant upgrade loader
-['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js','buyer-experience.js','cart-controls-fix.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-7';s.defer=false;document.head.appendChild(s)});
+// MINTFORGE worldwide country/listing/variant/cart upgrade loader
+['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js','buyer-experience.js','cart-experience-v2.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-8';s.defer=false;document.head.appendChild(s)});
