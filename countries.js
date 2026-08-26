275367dce8d7a69ab35fb661f02db31c93710cb0
ReeceSolana <110007863+reeceba@users.noreply.github.com>
2026-08-26 21:47:29 +1000

    Load clean V3 cart experience and retire conflicting cart patch

--- countries.js
+++ countries.js
@@ -1,2 +1,2 @@
 // MINTFORGE worldwide country/listing/variant/cart upgrade loader
-['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js','buyer-experience.js','cart-experience-v2.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-8';s.defer=false;document.head.appendChild(s)});
+['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js','buyer-experience.js','cart-experience-v3.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-9';s.defer=false;document.head.appendChild(s)});
