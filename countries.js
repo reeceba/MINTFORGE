1a0e0cdfdce16d989ce6b3fe7ef73ba90a536d6b
ReeceSolana <110007863+reeceba@users.noreply.github.com>
2026-08-26 18:36:12 +1000

    Load final worldwide shipping fix after legacy handlers

--- countries.js
+++ countries.js
@@ -1,2 +1,2 @@
 // MINTFORGE worldwide country/listing/variant upgrade loader
-['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src;s.defer=false;document.head.appendChild(s)});
+['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826';s.defer=false;document.head.appendChild(s)});
