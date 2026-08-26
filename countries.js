2513b85e17b9269b820981dc9285938983490710
ReeceSolana <110007863+reeceba@users.noreply.github.com>
2026-08-26 20:48:17 +1000

    Load buyer experience layer

--- countries.js
+++ countries.js
@@ -1,2 +1,2 @@
 // MINTFORGE worldwide country/listing/variant upgrade loader
-['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-3';s.defer=false;document.head.appendChild(s)});
+['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js','worldwide-final.js','simple-shipping.js','remove-listing.js','buyer-experience.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src+'?v=20260826-4';s.defer=false;document.head.appendChild(s)});
