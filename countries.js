67992b9b9325ef253afc3ed18f24b0f6d9d122ef
ReeceSolana <110007863+reeceba@users.noreply.github.com>
2026-08-26 18:20:11 +1000

    Wire worldwide shipping profile sync fix

--- countries.js
+++ countries.js
@@ -1,2 +1,2 @@
 // MINTFORGE worldwide country/listing/variant upgrade loader
-['listing-upgrade.js','variant-shop.js','soldout-fix.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src;s.defer=false;document.head.appendChild(s)});
+['listing-upgrade.js','variant-shop.js','soldout-fix.js','shipping-fix.js'].forEach(src=>{const s=document.createElement('script');s.src='./'+src;s.defer=false;document.head.appendChild(s)});
